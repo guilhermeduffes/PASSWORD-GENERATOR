86450d0c0055637e99eca3b4200062f7f07aabc7
guilherme <guiduffes@gmail.com>
2023-02-22 14:23:54 -0300

    first commit

--- main.js
+++ main.js
@@ -10,10 +10,6 @@ document.getElementById("passwordForm").addEventListener("submit", function(even
     let password = generatePassword(length, incluiMinuscula, incluiMaiuscula, incluiNumeros, incluiSimbolos);
     document.getElementById("password").textContent = password;
 
-    let passwordHistory = document.getElementById("historico");
-    let newPassword = document.createElement("p");
-    newPassword.textContent = password;
-    historico.appendChild(newPassword);
 });
 
 function generatePassword(length, incluiMinuscula, incluiMaiuscula, incluiNumeros, incluiSimbolos) {
@@ -37,23 +33,24 @@ function generatePassword(length, incluiMinuscula, incluiMaiuscula, incluiNumero
     }
 
     const element = document.getElementById("historico");
-    element.insertAdjacentHTML('beforeend', `<p><br>Senha gerada em${new Date().toLocaleString()}</p>`);
+    element.insertAdjacentHTML('beforeend', `<p><br> <b> &nbsp; ${password} </b> <br> <p style="font-weight: 100" > &nbsp; &nbsp; ${new Date().toLocaleString()} </p> </p>`);
 
     var element2 = document.getElementById("password");
     element2.style.padding = "10px 45px 10px 10px"
 
-
     return password;
 }
 
-
-
-const slider = document.getElementById("slider");
+const length = document.getElementById("length");
 const output = document.getElementById("slider-value");
-output.innerHTML = slider.value;
+output.innerHTML = length.value;
 
-slider.oninput = function() {
+length.oninput = function() {
   output.innerHTML = this.value;
 }
 
-
+function limparHistorico() {
+    document.getElementById("historico").innerHTML = "";
+    document.getElementById("password").innerHTML = ""
+    document.getElementById("password").style.padding = "0"
+}

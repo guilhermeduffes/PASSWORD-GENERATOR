51068aa2e2c90c3480af9333a27d8c308c41e8fe
guilherme <guiduffes@gmail.com>
2023-03-01 09:23:37 -0300

    sla qual o commit

--- main.js
+++ main.js
@@ -1,56 +1,79 @@
+var password = '';
+var length = document.getElementById("length").value;
+let includeLowercase = document.getElementById("includeLowercase").checked;
+let includeUppercase = document.getElementById("includeUppercase").checked;
+let includeNumbers = document.getElementById("includeNumbers").checked;
+let includeSymbols = document.getElementById("includeSymbols").checked;
+
+
 document.getElementById("passwordForm").addEventListener("submit", function(event) {
     event.preventDefault();
+    
+    length = document.getElementById("length").value;
+    includeLowercase = document.getElementById("includeLowercase").checked;
+    includeUppercase = document.getElementById("includeUppercase").checked;
+    includeNumbers = document.getElementById("includeNumbers").checked;
+    includeSymbols = document.getElementById("includeSymbols").checked;
 
-    let length = document.getElementById("length").value;
-    let incluiMinuscula = document.getElementById("incluiMinuscula").checked;
-    let incluiMaiuscula = document.getElementById("incluiMaiuscula").checked;
-    let incluiNumeros = document.getElementById("incluiNumeros").checked;
-    let incluiSimbolos = document.getElementById("incluiSimbolos").checked;
-
-    let password = generatePassword(length, incluiMinuscula, incluiMaiuscula, incluiNumeros, incluiSimbolos);
+    password = generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
     document.getElementById("password").textContent = password;
-
 });
 
-function generatePassword(length, incluiMinuscula, incluiMaiuscula, incluiNumeros, incluiSimbolos) {
+document.querySelector('.butao').addEventListener('click', function() {
+    let password = generatePassword(includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+    document.getElementById("password").textContent = password;
+});
+
+function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
     let charSet = "";
-    if (incluiMinuscula) {
+    length = document.getElementById("length").value;
+
+    if (includeLowercase) {
         charSet += "abcdefghijklmnopqrstuvwxyz";
     }
-    if (incluiMaiuscula) {
+    if (includeUppercase) {
         charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
-    if (incluiNumeros) {
+    if (includeNumbers) {
         charSet += "0123456789";
     }
-    if (incluiSimbolos) {
+    if (includeSymbols) {
         charSet += "!@#$%^&*()_+-=[]{}|;':\"<>,.?/\\";
     }
+    
 
-    let password = "";
+    let newPassword = "";
     for (let i = 0; i < length; i++) {
-        password += charSet.charAt(Math.floor(Math.random() * charSet.length));
+        newPassword += charSet.charAt(Math.floor(Math.random() * charSet.length));
     }
 
     const element = document.getElementById("historico");
-    element.insertAdjacentHTML('beforeend', `<p><br> <b> &nbsp; ${password} </b> <br> <p style="font-weight: 100" > &nbsp; &nbsp; ${new Date().toLocaleString()} </p> </p>`);
+    element.insertAdjacentHTML('beforeend', `<p><br> <b> &nbsp; ${newPassword} </b> <br> <p style="font-weight: 100" > &nbsp; &nbsp; ${new Date().toLocaleString()} </p> </p>`);
 
     var element2 = document.getElementById("password");
     element2.style.padding = "10px 45px 10px 10px"
 
-    return password;
+    return newPassword;
 }
 
-const length = document.getElementById("length");
+const lengthSlider = document.getElementById("length");
 const output = document.getElementById("slider-value");
-output.innerHTML = length.value;
+output.innerHTML = lengthSlider.value;
 
-length.oninput = function() {
-  output.innerHTML = this.value;
+lengthSlider.oninput = function() {
+    output.innerHTML = this.value;
 }
 
 function limparHistorico() {
     document.getElementById("historico").innerHTML = "";
-    document.getElementById("password").innerHTML = ""
-    document.getElementById("password").style.padding = "0"
+    document.getElementById("password").innerHTML = "";
+    document.getElementById("password").style.padding = "0";
 }
+
+document.querySelector('.butao').addEventListener('mousedown', function() {
+    this.classList.add('clicked');
+    setTimeout(() => {
+        this.classList.remove('clicked');
+    }, 300);
+});
+``
